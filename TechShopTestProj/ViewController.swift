0ef89be0f3b2eb2135e512d4518ba1bf8fd04810
HelgaPsycho <105391678+HelgaPsycho@users.noreply.github.com>
2022-12-04 15:48:27 +0300

    Initial Commit

---
+++ TechShopTestProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechShopTestProj
+//
+//  Created by Ольга Егорова on 04.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
